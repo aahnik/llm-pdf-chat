75ec8e60a88b4240f457e33fa0a710df50991c62
aahnik <daw@aahnik.dev>
2024-03-09 22:27:56 +0530

    update the generate response background task
    use the chain stored as global variable to give responses

--- server/tasks.py
+++ server/tasks.py
@@ -1,11 +1,10 @@
 # background tasks
-import asyncio
-import time
 
 from crud import create_new_msg
 from models import Message
+import lang
 
 
 async def generate_response(prompt: str):
-    await asyncio.sleep(10)
-    await create_new_msg(Message(username="assistant", message=f"Response to {prompt}"))
+    llm_response = lang.chain(prompt)
+    await create_new_msg(Message(username="assistant", message=llm_response["result"]))
