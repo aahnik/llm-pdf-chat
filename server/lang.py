733a485cae8bbf9c43497999b9a0b989e68d78cf
aahnik <daw@aahnik.dev>
2024-03-09 22:25:29 +0530

    fix lang.py
    - fix imports, import from langchain-community
    - fix returns of component functions
    - make loader functions to load global variables

--- server/lang.py
+++ server/lang.py
@@ -1,58 +1,61 @@
-from InstructorEmbedding import INSTRUCTOR
-from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatAnthropic
-from langchain.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts.chat import (
-    AIMessagePromptTemplate,
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
-)
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.tools import DuckDuckGoSearchRun
-from langchain.vectorstores import Chroma
-from PyPDF2 import PdfReader
+from langchain_community.chat_models import ChatAnthropic
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 
-CHROMA_PERSIST_DB_DIR = "db"
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from config import CONFIG
+from crud import get_llm_config
+
+CHROMA_PERSIST_DB_DIR = "chroma-db"
 
 
 def load_pdfs(look_dir):
-    loader = DirectoryLoader(look_dir, glob="./*.txt", loader_cls=TextLoader)
-
+    loader = PyPDFDirectoryLoader(look_dir)
     documents = loader.load()
-    len(documents)
+    return documents
 
 
 def process_pdfs(documents):
-    # splitting the text into
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
-
     instructor_embeddings = HuggingFaceInstructEmbeddings(
         model_name="hkunlp/instructor-xl", model_kwargs={"device": "cuda"}
     )
+    return texts, instructor_embeddings
 
 
-def setup_vectordb(embedding):
-
+def setup_vectordb(texts, embedding):
     vectordb = Chroma.from_documents(
         documents=texts, embedding=embedding, persist_directory=CHROMA_PERSIST_DB_DIR
     )
-
-    # making out retriever
-    retriever = vectordb.as_retriever(search_kwargs={"k": 7})
+    return vectordb.as_retriever(search_kwargs={"k": 7})
 
 
-def create_chain():
-    # create the chain to answer questions
-    llm = ChatAnthropic(model="claude-2", temperature=0.0)
+def create_chain(retreiver, model="claude-2", temp=0.0):
 
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True
+    llm = ChatAnthropic(model=model, temperature=temp)
+    return RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=retreiver, return_source_documents=True
     )
 
-    return qa_chain
+
+docs = None
+texts, embeddings = None, None
+retreiver = None
+chain = None
+
+
+async def load_chain():
+    global chain
+    model, temp = await get_llm_config()
+    chain = create_chain(retreiver, model, temp)
+
+
+async def process_docs():
+    global docs, texts, embeddings, retreiver
+    docs = load_pdfs(CONFIG.files_dir)
+    texts, embeddings = process_pdfs(docs)
+    retreiver = setup_vectordb(texts, embeddings)
